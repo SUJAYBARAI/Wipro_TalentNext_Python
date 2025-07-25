ca33e5777bf9732512c0b0064c6687d7ec162540
SUJAY BARAI <163174626+SUJAYBARAI@users.noreply.github.com>
2025-07-25 21:47:16 +0530

    Update Mini_Projects.py

--- Functions_Module_Packages/Mini_Projects.py
+++ Functions_Module_Packages/Mini_Projects.py
@@ -1,10 +1,9 @@
 # Project 1: Tech Module: Functions/Modules/Packages
 def sort_colors(color_sequence):
     colors = color_sequence.split('-')
-    colors.sort()  # Sorts alphabetically
+    colors.sort()  
     return '-'.join(colors)
 
-# Example usage:
 input_colors = input("Enter hyphen-separated colors: ")
 sorted_colors = sort_colors(input_colors)
 print("Sorted colors:", sorted_colors)
@@ -13,7 +12,7 @@ print("Sorted colors:", sorted_colors)
 # Project 2: 
 
 def is_palindrome(name):
-    name = name.replace(" ", "")  # Remove spaces
+    name = name.replace(" ", "")  
     return name == name[::-1]
 
 def count_the_vowels(name):
@@ -23,6 +22,6 @@ def count_the_vowels(name):
 def frequency_of_letters(name):
     freq = {}
     for char in name:
-        if char != ' ':  # Skip spaces
+        if char != ' ': 
             freq[char] = freq.get(char, 0) + 1
     return freq
